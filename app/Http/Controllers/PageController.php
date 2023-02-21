001762c4fa00c7f8962d6307e5005e47883580db
Marco 9726 <marcosarra9@gmail.com>
2023-02-21 17:39:59 +0100

    esercizio base svolto

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comic as Comic;
 
 class PageController extends Controller
 {
-    //
+	public function show()
+	{
+		$comics = Comic::all();
+
+		$headerMenu = config('headermenu');
+
+		$footerLists = config('footerlists');
+
+		$socialArray = config('social');
+
+		return view('comics', compact('headerMenu', 'footerLists', 'comics', 'socialArray'));
+	}
 }
